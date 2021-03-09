26cf2f3f09ee7d06b5d8112327179ff364c31b39
Shubham <shubhamkumar876@gmail.com>
2021-03-09 14:32:54 +0530

    Login component added!

---
+++ Frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId: '0oa3e8d1ptY9tPxzz5d6',
+        issuer: 'https://dev-4940645.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
